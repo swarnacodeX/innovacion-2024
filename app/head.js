0cae2cd84ecbca0d0f11eb162bb9db5be0239a01
Shubhangan Das <shubhangandas7366@gmail.com>
2024-03-04 15:44:47 +0530

    logo

--- app/head.js
+++ app/head.js
@@ -2,7 +2,7 @@ const Head = () => (
   <>
     <title>INNOVACION 2024</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/favicon.ico" />
+    <link rel="icon" href="\favicon.ico" />
   </>
 );
 
