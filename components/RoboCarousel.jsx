b24e2cb48e6cce3370990009e922d6da4e55e397
Shubhangan Das <shubhangandas7366@gmail.com>
2024-03-04 20:52:36 +0530

    abcd

--- components/RoboCarousel.jsx
+++ components/RoboCarousel.jsx
@@ -755,14 +755,15 @@ const RoboCarousel = () => {
         <motion.div
           variants={zoomIn(0.4, 1)}
           className="lg:block absolute -left-[-55%] top-[-40%]"
-        >
+        ><Link href="ROBOPACE Rule book.pdf">
           <Image
-          onClick={PDFROBO}
+          
             src="/5.png"
             alt="stamp" width={155}
             height={155}
             className="w-[155px] h-[155px] object-contain"
-          /></motion.div>
+          /></Link>
+          </motion.div>
       </motion.div>
      </motion.div> 
     </div>
@@ -821,14 +822,14 @@ const RoboCarousel = () => {
         <motion.div
           variants={zoomIn(0.4, 1)}
           className="lg:block absolute -left-[-55%] top-[-40%]"
-        >
+        ><Link href="/HELL IN A CELL Rulebook.pdf">
           <Image
           
             src="/5.png"
             alt="stamp" width={155}
             height={155}
             className="w-[155px] h-[155px] object-contain"
-          />
+          /></Link>
           </motion.div>
       </motion.div>
      </motion.div> 
